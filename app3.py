cb2e3dd5f8b395dfed9551694b8773d6ef18fbec
Chenxinjun488 <158443456+Chenxinjun488@users.noreply.github.com>
2024-06-25 18:32:25 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -3,9 +3,15 @@ import streamlit as st
 from pygwalker.api.streamlit import StreamlitRenderer
 
 
-uploaded_file = st.file_uploader("Your csv file")
+uploaded_file = st.file_uploader("Upload your file (CSV or Excel)", type=["csv", "xlsx"])
 
-if uploaded_file is not None :
-    df = pd.read_csv(uploaded_file)
+if uploaded_file is not None:
+    file_extension = uploaded_file.name.split('.')[-1].lower()
+    if file_extension == 'csv':
+        df = pd.read_csv(uploaded_file)
+    elif file_extension in ['xlsx', 'xls']:
+        df = pd.read_excel(uploaded_file)
+    else:
+        st.error("Unsupported file format. Please upload a CSV or Excel file.")
     pyg_app = StreamlitRenderer(df)
     pyg_app.explorer()
